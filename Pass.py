340cb5ade9fb39759261b079d7c4d62a9805ec61
Anas Shakeel <anasshakeel9011@gmail.com>
2025-04-13 23:11:23 +0500

    Add files via upload

---
+++ Pass.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import re
+
+st.set_page_config(page_title="Password Strength Checker", page_icon="🔒")
+
+st.title("🔐Password Strength Checker")
+st.markdown("""
+## welcome to the ultimate password strength checker!👋
+use this simple tool to check the strength of your password and get suggestions on how to make it stronger.
+            we will give helpful tips to create a **Strong Password** 🔒""")
+
+password = st.text_input("Enter your password" "🔑", type="password")
+
+feedback = []
+
+score = 0
+
+if password:
+    if len(password) >= 8:
+        score += 1
+    else :
+         feedback.append("❌Password should be at least 8 characters long.")
+
+    if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
+        score += 1
+    else:
+        feedback.append("❌Password should contain both upper and lower case characters.")    
+
+    if re.search(r'\d', password):
+        score += 1
+    else:
+        feedback.append("❌Password should contain at least one digit.")
+
+    if re.search(r"[!@#$%&*]", password):
+        score += 1
+    else:
+        feedback.append("❌Password should contain at least one special character[!@#$%&*].")
+    if score == 4:
+        feedback.append("✅ Password is strong.")   
+    elif score == 3:
+        feedback.append("your password medium strength. It could be stronger.")
+    else:
+        feedback.append("🔴Your password is weak. It could be stronger.")
+
+    if feedback:
+        st.markdown("## Improvements Suggestions")
+        for tip in feedback:
+            st.write(tip)
+
+else:
+   st.info("Please enter your password to get started.")       
+              
+
